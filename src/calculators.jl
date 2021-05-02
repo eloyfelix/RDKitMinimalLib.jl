add88de660f36ce0dd5554c5df55ace974b9e5a5
Eloy Félix <eloyfelix@gmail.com>
2021-05-02 10:02:20 +0100

    fix typos, init Ref{} values, export _as_bytes functions

--- src/calculators.jl
+++ src/calculators.jl
@@ -5,9 +5,9 @@ function get_morgan_fp(mol::Mol, details::Union{Dict{String,Any},Nothing}=nothin
     return unsafe_string(val)
 end
 
-function get_morgan_fp_as_bytes(mol::Mol, details::Union{Dict{String,Any},Nothing}=nothing)::string
+function get_morgan_fp_as_bytes(mol::Mol, details::Union{Dict{String,Any},Nothing}=nothing)::String
     details_json::String = jsonify_details(details)
-    n_bytes = Ref{Csize_t}()
+    n_bytes = Ref{Csize_t}(0)
     val = ccall((:get_morgan_fp_as_bytes, librdkitcffi), Cstring, (Cstring, Csize_t, Ref{Csize_t}, Cstring), mol.mol[], mol.mol_size[], n_bytes, details_json)
     return unsafe_string(val)
 end
@@ -18,9 +18,9 @@ function get_rdkit_fp(mol::Mol, details::Union{Dict{String,Any},Nothing}=nothing
     return unsafe_string(val)
 end
 
-function get_rdkit_fp_as_bytes(mol::Mol, details::Union{Dict{String,Any},Nothing}=nothing)::string
+function get_rdkit_fp_as_bytes(mol::Mol, details::Union{Dict{String,Any},Nothing}=nothing)::String
     details_json::String = jsonify_details(details)
-    n_bytes = Ref{Csize_t}()
+    n_bytes = Ref{Csize_t}(0)
     val = ccall((:get_rdkit_fp_as_bytes, librdkitcffi), Cstring, (Cstring, Csize_t, Ref{Csize_t}, Cstring), mol.mol[], mol.mol_size[], n_bytes, details_json)
     return unsafe_string(val)
 end
@@ -33,7 +33,7 @@ end
 
 function get_pattern_fp_as_bytes(mol::Mol, details::Union{Dict{String,Any},Nothing}=nothing)::String
     details_json::String = jsonify_details(details)
-    n_bytes = Ref{Csize_t}()
+    n_bytes = Ref{Csize_t}(0)
     val = ccall((:get_pattern_fp_as_bytes, librdkitcffi), Cstring, (Cstring, Csize_t, Ref{Csize_t}, Cstring), mol.mol[], mol.mol_size[], n_bytes, details_json)
     return unsafe_string(val)
 end
