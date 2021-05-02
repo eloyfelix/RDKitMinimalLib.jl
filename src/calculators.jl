4766485963cc6c737fb47bfdf245983eca288b32
Eloy Félix <eloyfelix@gmail.com>
2021-05-02 10:00:47 +0100

    add fingerprnts as_bytes

--- src/calculators.jl
+++ src/calculators.jl
@@ -18,7 +18,7 @@ function get_rdkit_fp(mol::Mol, details::Union{Dict{String,Any},Nothing}=nothing
     return unsafe_string(val)
 end
 
-function get_rdkit_fp_as_bytes(mol::Mol, details::Union{Dict{String,Any},Nothing}=nothing)::string
+function get_rdkit_fp_as_bytes(mol::Mol, details::Union{Dict{String,Any},Nothing}=nothing)::String
     details_json::String = jsonify_details(details)
     n_bytes = Ref{Csize_t}(0)
     val = ccall((:get_rdkit_fp_as_bytes, librdkitcffi), Cstring, (Cstring, Csize_t, Ref{Csize_t}, Cstring), mol.mol[], mol.mol_size[], n_bytes, details_json)
