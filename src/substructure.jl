8244a9d07b5b9652d1f93890be09ce80787a1797
Eloy Félix <eloyfelix@gmail.com>
2021-05-02 09:01:04 +0100

    substructure example

--- src/substructure.jl
+++ src/substructure.jl
@@ -1,12 +1,12 @@
 
-function get_substruct_match(mol::Mol, qmol::Mol, options::Union{Dict{String,Any},Nothing}=nothing)::String
+function get_substruct_match(mol::Mol, qmol::Mol, options::Union{Dict{String,Any},Nothing}=nothing)::Dict{String, Any}
     options_json::String = jsonify_details(options)
-    val = ccall((:get_substruct_match, librdkitcffi), Cstring, (Cstring, Csize_t, Cstring, Csize_t, Cstring), mol.mol, mol.mol_size[], qmol.mol, qmol.mol_size[], options_json)
-    return unsafe_string(val)
+    val = ccall((:get_substruct_match, librdkitcffi), Cstring, (Cstring, Csize_t, Cstring, Csize_t, Cstring), mol.mol[], mol.mol_size[], qmol.mol[], qmol.mol_size[], options_json)
+    return JSON.parse(unsafe_string(val))
 end
 
-function get_substruct_matches(mol::Mol, qmol::Mol, options::Union{Dict{String,Any},Nothing}=nothing)::String
+function get_substruct_matches(mol::Mol, qmol::Mol, options::Union{Dict{String,Any},Nothing}=nothing)::Dict{String, Any}
     options_json::String = jsonify_details(options)
-    val = ccall((:get_substruct_matches, librdkitcffi), Cstring, (Cstring, Csize_t, Cstring, Csize_t, Cstring), mol.mol, mol.mol_size[], qmol.mol, qmol.mol_size[], options_json)
-    return unsafe_string(val)
+    val = ccall((:get_substruct_matches, librdkitcffi), Cstring, (Cstring, Csize_t, Cstring, Csize_t, Cstring), mol.mol[], mol.mol_size[], qmol.mol[], qmol.mol_size[], options_json)
+    return JSON.parse(unsafe_string(val))
 end
