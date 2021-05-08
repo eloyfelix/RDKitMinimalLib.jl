5e73d05e0a61a09c81f63f1f2e3a98a33375a578
Eloy Félix <eloyfelix@gmail.com>
2021-05-08 17:44:19 +0100

    get some standarization docs ready, fix set_2d_coords_aligned

--- src/io.jl
+++ src/io.jl
@@ -184,7 +184,7 @@ end
 Get the InChIKey for a InChI.
 
 ```julia
-inchikey = get_inchikey_for_inchi(inchi)
+inchikey = get_inchikey_for_inchi("InChI=1S/C9H8O4/c1-6(10)13-8-5-3-2-4-7(8)9(11)12/h2-5H,1H3,(H,11,12)")
 ```
 """
 function get_inchikey_for_inchi(inchi::AbstractString)
