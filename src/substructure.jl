46d8aa79c4d6924deff5d80d6bef72cf4ffeae4a
Eloy Félix <eloyfelix@gmail.com>
2022-10-02 11:28:20 +0100

    fix substructure search docs #22

--- src/substructure.jl
+++ src/substructure.jl
@@ -1,32 +1,36 @@
 
 """
-    get_substruct_match(mol::Mol, qmol::Mol, options::Union{Dict{String,Any},Nothing}=nothing)::Dict{String, Any}
+get_substruct_match(mol::Mol, qmol::Mol, options::Union{Dict{String,Any},Nothing}=nothing)::Dict{String, Any}
 
 Gets a substructure match.
 
 ```julia
-smatch = get_substruct_match(mol)
+pattern = get_qmol("ccO")
+mol = get_mol("c1ccccc1O")
+smatch = get_substruct_match(mol, pattern)
 ```
 """
 function get_substruct_match(mol::Mol, qmol::Mol, options::Union{Dict{String,Any},Nothing}=nothing)::Dict{String, Any}
-    options_json::String = jsonify_details(options)
-    val::Cstring = ccall((:get_substruct_match, librdkitcffi), Cstring, (Cstring, Csize_t, Cstring, Csize_t, Cstring), mol.mol[], mol.mol_size[], qmol.mol[], qmol.mol_size[], options_json)
-    smatch = JSON.parse(unsafe_string_and_free(val))
-    return smatch
+options_json::String = jsonify_details(options)
+val::Cstring = ccall((:get_substruct_match, librdkitcffi), Cstring, (Cstring, Csize_t, Cstring, Csize_t, Cstring), mol.mol[], mol.mol_size[], qmol.mol[], qmol.mol_size[], options_json)
+smatch = JSON.parse(unsafe_string_and_free(val))
+return smatch
 end
 
 """
-    get_substruct_matches(mol::Mol, qmol::Mol, options::Union{Dict{String,Any},Nothing}=nothing)::Dict{String, Any}
+get_substruct_matches(mol::Mol, qmol::Mol, options::Union{Dict{String,Any},Nothing}=nothing)::Dict{String, Any}
 
 Gets all substructure matches.
 
 ```julia
-smatches = get_substruct_matches(mol)
+pattern = get_qmol("ccO")
+mol = get_mol("c1ccccc1O")
+smatches = get_substruct_matches(mol, pattern)
 ```
 """
 function get_substruct_matches(mol::Mol, qmol::Mol, options::Union{Dict{String,Any},Nothing}=nothing)::Vector{Any}
-    options_json::String = jsonify_details(options)
-    val::Cstring = ccall((:get_substruct_matches, librdkitcffi), Cstring, (Cstring, Csize_t, Cstring, Csize_t, Cstring), mol.mol[], mol.mol_size[], qmol.mol[], qmol.mol_size[], options_json)
-    smatches = JSON.parse(unsafe_string_and_free(val))
-    return smatches
+options_json::String = jsonify_details(options)
+val::Cstring = ccall((:get_substruct_matches, librdkitcffi), Cstring, (Cstring, Csize_t, Cstring, Csize_t, Cstring), mol.mol[], mol.mol_size[], qmol.mol[], qmol.mol_size[], options_json)
+smatches = JSON.parse(unsafe_string_and_free(val))
+return smatches
 end
