8be755179b8db04d740b985bbe7041e7fdfecdb9
Sebastian Schmitt <se.s.schmitt@gmail.com>
2026-07-30 10:02:32 +0200

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -45,7 +45,7 @@ M  END
     @test get_cxsmiles(mol) == "CC(=O)Oc1ccccc1C(=O)O |(11.7423,-4.5949,;11.0273,-4.1837,;10.3136,-4.5972,;11.026,-3.3588,;10.311,-2.9474,;9.5946,-3.3607,;8.8798,-2.9479,;8.881,-2.1206,;9.5928,-1.7078,;10.3081,-2.117,;11.021,-1.7018,;11.7369,-2.1116,;11.0178,-0.8769,)|"
     @test occursin("V2000", get_molblock(mol))
     @test occursin("V3000", get_v3kmolblock(mol))
-    @test occursin("commonchem", get_json(mol))
+    @test occursin("rdkitjson", get_json(mol))
     @test get_inchi(mol) == "InChI=1S/C9H8O4/c1-6(10)13-8-5-3-2-4-7(8)9(11)12/h2-5H,1H3,(H,11,12)"
     @test get_inchi_for_molblock(molblockv2000) == "InChI=1S/C9H8O4/c1-6(10)13-8-5-3-2-4-7(8)9(11)12/h2-5H,1H3,(H,11,12)"
     @test get_inchikey_for_inchi("InChI=1S/C9H8O4/c1-6(10)13-8-5-3-2-4-7(8)9(11)12/h2-5H,1H3,(H,11,12)") == "BSYNRYMUTXBXSQ-UHFFFAOYSA-N"
@@ -97,7 +97,58 @@ end
     tb = get_topological_torsion_fp_as_bytes(mol)
     @test sum([count_ones(b) for b in tb]) == 18
 
-    @test get_descriptors(mol) == Dict{String, Any}("CrippenMR" => 44.7103, "lipinskiHBD" => 1.0, "kappa1" => 9.2496, "kappa3" => 2.29741, "NumHBD" => 1.0, "NumSpiroAtoms" => 0.0, "chi1n" => 3.61745, "FractionCSP3" => 0.11111, "chi0n" => 6.98135, "NumUnspecifiedAtomStereoCenters" => 0.0, "NumAmideBonds" => 0.0, "NumAromaticHeterocycles" => 0.0, "NumAtoms" => 21.0, "tpsa" => 63.6, "NumHeteroatoms" => 4.0, "Phi" => 2.63916, "exactmw" => 180.04225, "hallKierAlpha" => -1.83999, "CrippenClogP" => 1.31009, "chi3v" => 1.37115, "NumSaturatedHeterocycles" => 0.0, "NumAromaticRings" => 1.0, "labuteASA" => 74.75705, "amw" => 180.15899, "NumAliphaticHeterocycles" => 0.0, "NumAtomStereoCenters" => 0.0, "chi4v" => 0.88717, "NumRotatableBonds" => 2.0, "lipinskiHBA" => 4.0, "NumRings" => 1.0, "chi0v" => 6.98135, "NumHeterocycles" => 0.0, "NumHeavyAtoms" => 13.0, "chi3n" => 1.37115, "chi1v" => 3.61745, "NumBridgeheadAtoms" => 0.0, "kappa2" => 3.70925, "chi2n" => 1.37115, "NumHBA" => 3.0, "chi4n" => 0.88717, "NumSaturatedRings" => 0.0, "NumAliphaticRings" => 0.0, "chi2v" => 1.37115)
+    descs = get_descriptors(mol)
+    ref_descs = [
+        "CrippenMR" => 44.7103,
+        "lipinskiHBD" => 1.0,
+        "kappa1" => 9.2496,
+        "kappa3" => 2.29741,
+        "NumHBD" => 1.0,
+        "NumSpiroAtoms" => 0.0,
+        "chi1n" => 3.61745,
+        "FractionCSP3" => 0.11111,
+        "chi0n" => 6.98135,
+        "NumUnspecifiedAtomStereoCenters" => 0.0,
+        "NumAmideBonds" => 0.0,
+        "NumAromaticHeterocycles" => 0.0,
+        "NumAtoms" => 21.0,
+        "tpsa" => 63.6,
+        "NumHeteroatoms" => 4.0,
+        "Phi" => 2.63916,
+        "exactmw" => 180.04225,
+        "hallKierAlpha" => -1.83999,
+        "CrippenClogP" => 1.31009,
+        "chi3v" => 1.37115,
+        "NumSaturatedHeterocycles" => 0.0,
+        "NumAromaticRings" => 1.0,
+        "labuteASA" => 74.75705,
+        "amw" => 180.15899,
+        "NumAliphaticHeterocycles" => 0.0,
+        "NumAtomStereoCenters" => 0.0,
+        "chi4v" => 0.88717,
+        "NumRotatableBonds" => 2.0,
+        "lipinskiHBA" => 4.0,
+        "NumRings" => 1.0,
+        "chi0v" => 6.98135,
+        "NumHeterocycles" => 0.0,
+        "NumHeavyAtoms" => 13.0,
+        "chi3n" => 1.37115,
+        "chi1v" => 3.61745,
+        "NumBridgeheadAtoms" => 0.0,
+        "kappa2" => 3.70925,
+        "chi2n" => 1.37115,
+        "NumHBA" => 3.0,
+        "chi4n" => 0.88717,
+        "NumSaturatedRings" => 0.0,
+        "NumAliphaticRings" => 0.0,
+        "chi2v" => 1.37115
+    ]
+    @test typeof(descs) == Dict{String, Any}
+    @testset "descriptors" begin
+        for (k,v) in ref_descs
+            @test v ≈ descs[k] atol=1e-4
+        end
+    end
 end
 
 @testset "standardization" begin
